768379d1b39d3117d3866d82d94ef6376c2caa02
baontraan <baotran221003@gmail.com>
2024-07-09 04:02:54 +0700

    update 9/7

---
+++ product.js
@@ -0,0 +1,45 @@
+let empty = [];
+const product = [
+  {
+    id: 1,
+    name: "Vở Dash Line lò xo kép bìa nhựa B5 200 trang",
+    src: "https://pos.nvncdn.com/7a6d8f-102734/ps/20240424_DkfC0x3cDk.jpeg",
+    price: "36,860",
+    sale: "-3%",
+  },
+  {
+    id: 2,
+    name: "Vở kẻ ngang chấm B5 200tr Klong",
+    src: "https://pos.nvncdn.com/7a6d8f-102734/ps/20220901_tr4Gdm3QstNcQo6iO0Axiu2F.jpg",
+    price: "32,000",
+    sale: "-3%",
+  },
+  {
+    id: 3,
+    name: "Vở kẻ ngang chấm B5 120tr Klong",
+    src: "https://pos.nvncdn.com/7a6d8f-102734/ps/20220901_NJC1DvBGqd5UDtt4wE2ffutj.jpg",
+    price: "20,000",
+    sale: "-3%",
+  },
+  {
+    id: 4,
+    name: "Sticker chữ ánh bạc cổ điển STI57",
+    src: "https://pos.nvncdn.com/7a6d8f-102734/ps/20220511_BGmTBSS993CYuBSvyIUfmNAi.png",
+    price: "7,500",
+    sale: "-3%",
+  },
+  {
+    id: 5,
+    name: "Kệ giỏ nhựa mini K37",
+    src: "https://pos.nvncdn.com/7a6d8f-102734/ps/20211125_w196Z2qa7Yz6s3KseAVjalTb.jpg",
+    price: "54,000",
+    sale: "-3%",
+  },
+  {
+    id: 6,
+    name: "Sổ còng caro bìa nhựa A5 100 tờ",
+    src: "https://pos.nvncdn.com/7a6d8f-102734/ps/20210621_zkM2XD6Yft9TI90cJS1O2kB0.jpeg",
+    price: "36,860",
+    sale: "-3%",
+  },
+];
